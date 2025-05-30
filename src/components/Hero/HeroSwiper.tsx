7ae51d571ea6b06e3d03cdb0b6ce6834e7e95bd0
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-05-31 03:44:41 +0500

    ""

--- src/components/Hero/HeroSwiper.tsx
+++ src/components/Hero/HeroSwiper.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import './swiper/css/css.css'
 import './swiper/css/effect-cards.css'
 import useGetHooks from '../Hooks/useGetHooks'
+import { Skeleton } from '@/components/ui/skeleton'
 
 type Center = {
 	data: {
@@ -15,9 +16,24 @@ type Center = {
 
 export default function HeroSwiper() {
 	const url = import.meta.env.VITE_API_URL
-	const {data, isLoading, error} = useGetHooks<Center>(`${url}/centers`)
+	const { data, isLoading, error } = useGetHooks<Center>(`${url}/centers`)
 
-	if (isLoading) return <p>Loading...</p>
+	if (isLoading) {
+		return (
+			<div className='flex gap-6'>
+					<div
+						className='w-full h-[250px] bg-white rounded-xl shadow-md p-4 flex flex-col justify-between'
+					>
+						<Skeleton className='h-[240px] w-full rounded-lg' />{' '}
+						<div className='space-y-3 mt-4'>
+							<Skeleton className='h-4 w-3/4' /> 
+							<Skeleton className='h-4 w-1/2' /> 
+						</div>
+					</div>
+			</div>
+		)
+	}
+
 	if (error) return <p>Error: {error.message}</p>
 
 	const center = data?.data
@@ -36,9 +52,15 @@ export default function HeroSwiper() {
 		>
 			{center?.map(data => (
 				<SwiperSlide className='group flex-col' key={data?.id}>
-					<img className='h-full bg-contain' src={data?.image} alt={data?.name} />
-          <div className="opacity-0 group-hover:opacity-100 transition-all duration-500 fixed w-full h-full bg-[#000]/50"></div>
-					<p className='font-["Playwrite_HU"] absolute opacity-0 group-hover:opacity-100 transition-all duration-500 text-center px-[20px]'>{data?.name}</p>
+					<img
+						className='h-full bg-contain'
+						src={data?.image}
+						alt={data?.name}
+					/>
+					<div className='opacity-0 group-hover:opacity-100 transition-all duration-500 fixed w-full h-full bg-[#000]/50'></div>
+					<p className='font-["Playwrite_HU"] absolute opacity-0 group-hover:opacity-100 transition-all duration-500 text-center px-[20px]'>
+						{data?.name}
+					</p>
 				</SwiperSlide>
 			))}
 		</Swiper>
