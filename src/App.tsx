c05a591f90759acfafe146750e2e35b5c0ee9dcd
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-06-09 13:50:39 +0500

    ""

--- src/App.tsx
+++ src/App.tsx
@@ -12,35 +12,37 @@ import ElonPage from './pages/Elon/Elon'
 import NotFaundPage from './pages/NotFaund/NotFaundPage'
 import ResurslarPage from './pages/Resurslar/Resurslar'
 import RefreshToken from './components/Auth/RefreshToken'
-import ProtectedRoute from './ProtectedRoute'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  const isAuth = Boolean(localStorage.getItem('token'))
-
-  return (
-    <>
-       <ToastContainer />
-      <RefreshToken />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/otp' element={<OtpPage />} />
-        <Route path='/center/:id' element={<ProductsDetailsPage />} />
-        <Route path='/ceo' element={<CeoCreate />} />
-        <Route path='/elon' element={<ElonPage />} />
-        <Route path='/resurs' element={<ResurslarPage />} />
-
-        <Route element={<ProtectedRoute isAuth={isAuth} />}>
-          <Route path='/profile' element={<ProfilePage />}/>
-        </Route>
-        <Route path='*' element={<NotFaundPage />} />
-      </Routes>
-      <Footer />
-    </>
+	fetch('http://34.9.169.36:4000/api-docs/centers').then(res =>
+		console.log('Fetch_data', res)
+	).catch(error => 
+    console.log(error)
   )
+
+	return (
+		<>
+			<ToastContainer />
+			<RefreshToken />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/register' element={<RegisterPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/otp' element={<OtpPage />} />
+				<Route path='/center/:id' element={<ProductsDetailsPage />} />
+				<Route path='/ceo' element={<CeoCreate />} />
+				<Route path='/elon' element={<ElonPage />} />
+				<Route path='/resurs' element={<ResurslarPage />} />
+
+				<Route path='/profile' element={<ProfilePage />} />
+
+				<Route path='*' element={<NotFaundPage />} />
+			</Routes>
+			<Footer />
+		</>
+	)
 }
 
 export default App
